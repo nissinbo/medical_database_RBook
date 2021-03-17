c9169579be79bc111e63c17afa600dca0dbaddbb
nissinbo <43395303+nissinbo@users.noreply.github.com>
2021-03-17 23:07:12 +0900

    modify "sample_data_generator & preprocess"

--- data/sample_data_generator.R
+++ data/sample_data_generator.R
@@ -1,18 +1,20 @@
 library(tidyverse)
 library(data.table)
+library(comorbidity)
+library(wakefield)
 
 set.seed(1234)
 
 n <- 10
 
-sample_patient <- data.frame(id = 1:n) %>% 
+patient <- data.frame(id = 1:n) %>% 
   mutate(gender = sample(x = c("F", "M"), size = n, replace = TRUE)) %>% 
   mutate(start_date = sample(seq(as.Date("2010-01-01"), as.Date("2020/12/31"), "day"), n))
 
 
 drug_row_n <- sample(n:(n * 100), 1)
 
-sample_drug <- data.frame(id = c(1:n, sample(1:n, drug_row_n, replace = TRUE))) %>% 
+drug <- data.frame(id = c(1:n, sample(1:n, drug_row_n, replace = TRUE))) %>% 
   left_join(sample_patient %>% select(id, start_date), "id") %>% 
   mutate(date = sample(seq(as.Date("2010-01-01"), as.Date("2020/12/31"), "day"), nrow(.))) %>% 
   filter(start_date <= date) %>% 
@@ -24,15 +26,23 @@ sample_drug <- data.frame(id = c(1:n, sample(1:n, drug_row_n, replace = TRUE))) 
 
 disease_row_n <- sample(n:(n * 100), 1)
 
-sample_disease <- data.frame(id = c(1:n, sample(1:n, disease_row_n, replace = TRUE))) %>% 
+disease <- data.frame(id = c(1:n, sample(1:n, disease_row_n, replace = TRUE))) %>% 
   left_join(sample_patient %>% select(id, start_date), "id") %>% 
   mutate(date = sample(seq(as.Date("2010-01-01"), as.Date("2020/12/31"), "day"), nrow(.))) %>% 
   filter(start_date <= date) %>% 
   select(!start_date) %>% 
   arrange(id, date) %>% 
-  mutate(icd10code = paste0(sample(LETTERS[1:3], nrow(.), replace = TRUE), sample(rep(0:10) %>% str_pad(2, pad ="0"), nrow(.), replace = TRUE)))
+  mutate(., icd10code = sample_diag(nrow(.))) %>% 
+  r_na(2)
 
 
-fwrite(sample_patient, "sample_patient.csv")
-fwrite(sample_drug, "sample_drug.csv")
-fwrite(sample_disease, "sample_disease.csv")
+fwrite(patient, "data/patient.csv")
+fwrite(drug, "data/drug.csv")
+fwrite(disease, "data/disease.csv")
+
+
+# data_gh <- "https://raw.githubusercontent.com/nissinbo/medical_database_RBook/master/data/"
+
+# patient <- read.csv(paste0(data_gh, "patient.csv"))
+# drug <- read.csv(paste0(data_gh, "drug.csv"))
+# disease <- read.csv(paste0(data_gh, "disease.csv"))
